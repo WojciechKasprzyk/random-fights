62461741c3fb3844ddce25d248dac0ebcf1584f3
Wojciech Kasprzyk <wojciech.kasprzyk@loycon.pl>
2020-05-12 14:26:29 +0200

    score table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { SwapiApiService } from './services/swapi-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'random-fights';
+  playedAlready = false;
 
   constructor(private swapiApiService: SwapiApiService) {}
+
+  play() {
+    this.playedAlready = true;
+  }
 
   ngOnInit() {
     this.swapiApiService.getRandomStarship()
